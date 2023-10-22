4eb7339151a8f4a67dc310a18be1c665dd3a23d5
Przemek Ujda <przemek781@gmail.com>
2023-10-23 00:29:58 +0200

    map page added

--- nav.js
+++ nav.js
@@ -12,3 +12,16 @@ navToggle.addEventListener('click', () => {
         navToggle.setAttribute('aria-expanded', false)
     }
 })
+
+function initialize() {
+
+    var map = new google.maps.Map(document.getElementById("map-canvas"), mapOptions);
+  
+    // Resize stuff...
+    google.maps.event.addDomListener(window, "resize", function() {
+      var center = map.getCenter();
+      google.maps.event.trigger(map, "resize");
+      map.setCenter(center); 
+    });
+  
+  }
